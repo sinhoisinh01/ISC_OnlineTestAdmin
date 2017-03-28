c5dfee219594b0fc320179cbb6ee260462788695
hggntg <hggntg@gmail.com>
2017-03-28 10:50:06 +0700

    add show subjects list page

---
+++ scripts/controllers/subjectsController.js
@@ -0,0 +1,39 @@
+app.controller('SubjectsController', function($scope, DTOptionsBuilder){
+	// Subjects only have children, not grandchildren.
+	subjects = 
+	[
+		{
+			sub_id: 1, subid: null, subname:'Khoa học',
+			childsubjects:[
+				{sub_id: 2, subid: 1, subname:'Toán'},
+				{sub_id: 3, subid: 1, subname:'Lý'}
+			]
+		},
+		{
+			sub_id: 4, subid: null, subname:'Thất học',
+			childsubjects:[
+				{sub_id: 5, subid: 4, subname:'Boxing'},
+				{sub_id: 6, subid: 4, subname:'Muay Thai'}
+			]
+		},
+
+	]
+	array = [];
+	subjectsLength = subjects.length;
+	for(i = 0; i < subjectsLength; i++)
+    {
+        subjects[i].level = 1;
+        childSubjectsLength = subjects[i].childsubjects.length;
+        temp = JSON.parse(JSON.stringify(subjects[i]));
+        delete temp.childsubjects;
+        array.push(temp);
+
+        for( j = 0; j < childSubjectsLength; j++){
+        	childSubject = subjects[i].childsubjects[j];
+        	childSubject.level = 2;
+        	array.push(childSubject);
+        }
+    }
+    //console.log(array);
+	$scope.subjects = array;
+});
