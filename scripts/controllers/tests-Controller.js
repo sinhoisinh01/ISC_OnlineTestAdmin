151f6b024cca4ef3cc7577dd250966f5d5af7bcc
ngodonghong@gmail.com <ngodonghong@gmail.com>
2017-03-28 22:45:49 +0700

    Add Detail Test ver 0

--- scripts/controllers/tests-Controller.js
+++ scripts/controllers/tests-Controller.js
@@ -83,6 +83,59 @@ app.controller("TestController", function($scope,$http,$route,DTOptionsBuilder) 
 	];
 });
 
-app.controller("TestDetailController", function($scope,$http,$route) {
-	
+app.controller("TestDetailController", function($scope,$http,$route,DTOptionsBuilder) {
+	$scope.Exam_Takers = [
+		{
+			EXAT_ID : 1,
+			EXATID : 'DCT11245',
+			EXTFIRSTNAME : 'Chiến Văn',
+			EXTLASTNAME : 'Nguyễn',
+			EXTGENDER : 1,
+			EXATDOB : '1993/12/2',
+			EXATADDRESS : 'TPHCM',
+			EXATEMAIL : 'abd@gmail.com',
+			EXATPHONE : '01848484842',
+			ANSCORRECT : 153,
+			ANSSCORE : 654,
+		},
+		{
+			EXAT_ID : 2,
+			EXATID : 'DAT88788',
+			EXTFIRSTNAME : 'Tiến A',
+			EXTLASTNAME : 'Nguyễn',
+			EXTGENDER : 1,
+			EXATDOB : '1993/12/2',
+			EXATADDRESS : 'TPHCM',
+			EXATEMAIL : 'abd@gmail.com',
+			EXATPHONE : '01848484842',
+			ANSCORRECT : 100,
+			ANSSCORE : 435,
+		},
+		{
+			EXAT_ID : 3,
+			EXATID : 'DUT422345',
+			EXTFIRSTNAME : 'Tuấn',
+			EXTLASTNAME : 'Lê',
+			EXTGENDER : 1,
+			EXATDOB : '1995/12/2',
+			EXATADDRESS : 'TPHCM',
+			EXATEMAIL : 'abd@gmail.com',
+			EXATPHONE : '01848484842',
+			ANSCORRECT : 80,
+			ANSSCORE : 400,
+		},
+		{
+			EXAT_ID : 4,
+			EXATID : 'AVU12457',
+			EXTFIRSTNAME : 'Chính',
+			EXTLASTNAME : 'Tạ',
+			EXTGENDER : 1,
+			EXATDOB : '1995/12/2',
+			EXATADDRESS : 'TPHCM',
+			EXATEMAIL : 'abd@gmail.com',
+			EXATPHONE : '01848484842',
+			ANSCORRECT : 168,
+			ANSSCORE : 800,
+		}
+	];
 });
