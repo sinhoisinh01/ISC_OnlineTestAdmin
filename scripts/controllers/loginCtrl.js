e8fb2970df3b95e5bfcb620e617037ffa0c08ab7
sinhoisinh01 <doanphucsinh@gmail.com>
2017-04-27 00:31:38 +0700

    Allow Signout

--- scripts/controllers/loginCtrl.js
+++ scripts/controllers/loginCtrl.js
@@ -1,5 +1,7 @@
 app.controller('LoginController', function($scope,$cookies,LoginFactory){
     $scope.login = function(){
+            frontendBaseURL = "http://127.0.0.1:8887/";
+            console.log();
             var userName = $scope.userid;
             var password = $scope.password;
             LoginFactory.login(userName,password).then(function(response){
@@ -7,6 +9,7 @@ app.controller('LoginController', function($scope,$cookies,LoginFactory){
                 accessToken : response.data.access_token,
                 refreshToken : response.data.refresh_token
               });
+              window.location.href = frontendBaseURL + "index.html#!";
             },function(error){
               console.log(error);
               location.reload();
