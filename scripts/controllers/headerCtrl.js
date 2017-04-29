ee48e1fc7e7bd44ec208654cafd9895736af6406
sinhoisinh01 <doanphucsinh@gmail.com>
2017-04-29 10:40:42 +0700

    Show Username on top right corner when loged in

--- scripts/controllers/headerCtrl.js
+++ scripts/controllers/headerCtrl.js
@@ -1,4 +1,10 @@
-app.controller('HeaderController', function($scope,$cookies,LoginFactory, frontendBaseURL){
+/*
+ * Author: Đoàn Phúc Sinh
+ * Description: Controller for includes/header.html
+ */
+app.controller('HeaderController', function($scope, $cookies, LoginFactory, frontendBaseURL){
+    $scope.username = $cookies.getObject("user").username;
+
     $scope.logout = function(){
       $cookies.remove("user");
       window.location.href = frontendBaseURL + "login.html#!";
--- scripts/controllers/loginCtrl.js
+++ scripts/controllers/loginCtrl.js
@@ -1,20 +1,18 @@
 app.controller('LoginController', function($scope,$cookies,LoginFactory){
     $scope.login = function(){
-            //frontendBaseURL = "http://127.0.0.1:8887/";
-            console.log();
-            var userName = $scope.userid;
-            var password = $scope.password;
+      var userName = $scope.username;
+      var password = $scope.password;
 
-            LoginFactory.login(userName,password).then(function(response){
-            
-              $cookies.putObject("user",{
-                accessToken : response.data.access_token,
-                refreshToken : response.data.refresh_token
-              });
-              window.location.href = "./";
-            },function(error){
-              console.log(error);
-              // location.reload();
-            });
+      LoginFactory.login(userName,password).then(function(response){
+        $cookies.putObject("user",{
+          username: $scope.username,
+          accessToken : response.data.access_token,
+          refreshToken : response.data.refresh_token
+        });
+        window.location.href = "./";
+      },function(error){
+        console.log(error);
+        // location.reload();
+      });
     };
 });
