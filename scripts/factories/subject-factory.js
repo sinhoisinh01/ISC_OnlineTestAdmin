76ff0290973e59d880c6eec215625e8a994a53b4
giangthanhhai17@gmail.com <giangthanhhai17@gmail.com>
2017-05-04 09:27:14 +0700

    Not important

--- scripts/factories/subject-factory.js
+++ scripts/factories/subject-factory.js
@@ -9,7 +9,7 @@ app.factory("SubjectFactory",function(APIFactory){
 
 
 		findById : function( id, success = null, fail = null ){
-      return APIFactory.get(APIFactory.apiName("GET_SUBJECT_BY_ID"),[id],success,fail);
+      		return APIFactory.get(APIFactory.apiName("GET_SUBJECT_BY_ID"),[id],success,fail);
 		},
 
 		remove : function(id,success = null, fail = null){
